79e94e1bcbb92a9d7ef4358fe37a72416c98c15b
Dawei <davidyin@bu.edu>
2022-08-11 15:44:10 -0700

    hello world

---
+++ Websites/davsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mki9h$um^5hgqa#&*&-ketf%6j%4-gq0nliq=xffz82k6c(q#0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
